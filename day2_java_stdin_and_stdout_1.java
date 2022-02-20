860afab9a674c01b3aa0d3ff20fa796400d9880a
Sulfatma <100036853+Sulfatma@users.noreply.github.com>
2022-02-20 23:15:07 +0800

    Add files via upload

---
+++ day2_java_stdin_and_stdout_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class day2_java_stdin_and_stdout_1{
+	public static void main(String[]  args){
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int d = scan.nextInt();
+		int r = scan.nextInt();
+		
+		System.out.println(a);
+		System.out.println(d);
+		System.out.println(r);
+
+    }
+   
+}
